7cbd52621394e01205874d9309afaf77c945aa24
Lisgevan <episimo@hotmail.com>
2021-12-11 15:21:33 +0200

    React and Redux completed

--- 00_Testing/test.js
+++ 00_Testing/test.js
@@ -1,3 +1,9 @@
-document.querySelector("button").addEventListener("click", function (evt) {
-	console.log(evt);
-});
+function returnDay(num) {
+	const days = ["monday", "tuesday", "wenesday", "thursday", "friday", "saturady", "sunday"];
+	const day = days[num - 1];
+	return day ? day : null;
+}
+
+let result = returnDay(1);
+console.log(result);
+alert(result);
